b7ba0bef90e4da1ef0d93f2f1496416b2cd3a4de
Ethan Vrhel <ethanvrhel@gmail.com>
2022-01-18 11:27:34 -0800

    Updates to wording and link styling

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 let coll = document.getElementsByClassName("collapsible");
 
 for (let i = 0; i < coll.length; i++) {
+	// for each button, add an event listener to toggle its drop-down
 	coll[i].addEventListener("click", function () {
 		this.classList.toggle("active");
-		let content = this.nextElementSibling;
+		let content = this.nextElementSibling; // next element should be a div
+
+		// set its height
 		if (content.style.maxHeight)
 			content.style.maxHeight = null;
 		else
